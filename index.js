3cc97c361f70d7dc8be998a82760dfecb46af2fc
Aashish <aashishm97@knights.ucf.edu>
2021-03-07 17:08:30 -0500

    Working off WMT site

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const puppeteer = require("puppeteer");
 
-(async () => {
-    const browser = await puppeteer.launch({headless: false});
-    const page = await browser.newPage();
-    
-    await page.goto("https://bestbuy.com");
-    const title = await page.title();
-    const url = await page.url()
+try {
+    (async () => {
+        const browser = await puppeteer.launch({headless: false});
+        const page = await browser.newPage();
+        
+        // Go to website
+        await page.goto("https://www.walmart.com/ip/Microsoft-Xbox-One-S-1TB-All-Digital-Edition-Console-with-Xbox-One-Wireless-Controller-Renewed/604685632");
 
-    console.log(title, url)
-})();
+        // Add to cart
+        await page.click('button.prod-ProductCTA--primary')
+        await page.waitForSelector('.Cart-PACModal-ItemInfoContainer')
+
+        // Checkout process
+        await page.click('button.checkoutBtn')
+
+        // Here we are checking out without an account
+        
+        await page.click('#new-guest-continue-button')
+
+        
+    
+    })();
+} catch (err) {
+    console.log(err);
+}
